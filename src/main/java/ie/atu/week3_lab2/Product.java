e7c3c9736b4920ab990ac48f78761e7bc3d3c45c
Shine Sujith <G00414454@atu.ie>
2024-10-02 14:32:29 +0100

    added method to display products

--- src/main/java/ie/atu/week3_lab2/Product.java
+++ src/main/java/ie/atu/week3_lab2/Product.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Product {
     private String name;
-    private String category;
+    private String description;
     private int price;
     private int id;
 }
--- src/main/java/ie/atu/week3_lab2/ProductController.java
+++ src/main/java/ie/atu/week3_lab2/ProductController.java
@@ -3,10 +3,19 @@ package ie.atu.week3_lab2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class ProductController {
-    @GetMapping
-    public String getProducts(){
-        return "products";
+    private List<Product> products = new ArrayList<>();
+    public ProductController() {
+        products.add(new Product("Tv", "made by sony", 899, 100));
+        products.add(new Product("Radio", "made by samsung", 99, 101));
+    }
+
+    @GetMapping("/getProducts")
+    public List<Product> getProducts(){
+        return products;
     }
 }
