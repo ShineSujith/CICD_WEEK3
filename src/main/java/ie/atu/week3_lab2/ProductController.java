bfe223297a41443098d24aa57219e02e0c591b51
Shine Sujith <G00414454@atu.ie>
2024-10-07 21:13:37 +0100

    added extra products to the arraylist

--- src/main/java/ie/atu/week3_lab2/ProductController.java
+++ src/main/java/ie/atu/week3_lab2/ProductController.java
@@ -12,6 +12,13 @@ public class ProductController {
     public ProductController() {
         products.add(new Product("Tv", "made by sony", 899, 100));
         products.add(new Product("Radio", "made by samsung", 99, 101));
+        products.add(new Product("example1", "test1", 99, 102));
+        products.add(new Product("example2", "test2", 99, 103));
+        products.add(new Product("example3", "test3", 99, 104));
+        products.add(new Product("example4", "test4", 99, 105));
+        products.add(new Product("example5", "test5", 99, 106));
+        products.add(new Product("example6", "test6", 99, 107));
+        products.add(new Product("example7", "test7", 99, 108));
     }
 
     @GetMapping("/getProducts")
